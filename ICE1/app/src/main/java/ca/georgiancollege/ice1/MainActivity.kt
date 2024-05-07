11b4888662370510d63d09f09d62c7d6159a1c79
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-05-07 14:45:18 -0400

    modified helloWorldTextView programatically

--- ICE1/app/src/main/java/ca/georgiancollege/ice1/MainActivity.kt
+++ ICE1/app/src/main/java/ca/georgiancollege/ice1/MainActivity.kt
@@ -1,20 +1,29 @@
 package ca.georgiancollege.ice1
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val helloWorldTextView = findViewById<TextView>(R.id.helloWorldTextView)
+        helloWorldTextView.text = getString(R.string.good_bye_string)
     }
 }
