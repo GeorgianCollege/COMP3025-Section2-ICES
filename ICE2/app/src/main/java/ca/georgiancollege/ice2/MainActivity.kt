bdc9ea5f469dc4c3a097b581f69804374d0bedc8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-05-14 13:17:51 -0400

    added Project files for ICE2

---
+++ ICE2/app/src/main/java/ca/georgiancollege/ice2/MainActivity.kt
@@ -0,0 +1,29 @@
+package ca.georgiancollege.ice2
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val helloWorldString = findViewById<TextView>(R.id.helloWorldTextView)
+        helloWorldString.text = getString(R.string.good_bye_world)
+    }
+}
