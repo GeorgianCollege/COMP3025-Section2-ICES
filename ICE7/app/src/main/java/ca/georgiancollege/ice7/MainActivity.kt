5c3ef9ea59156019bbc9bb35b300d70d9ec1fcab
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-02 13:30:39 -0400

    added mock data to the recyclerView

--- ICE7/app/src/main/java/ca/georgiancollege/ice7/MainActivity.kt
+++ ICE7/app/src/main/java/ca/georgiancollege/ice7/MainActivity.kt
@@ -1,10 +1,8 @@
 package ca.georgiancollege.ice7
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import ca.georgiancollege.ice7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
@@ -16,6 +14,22 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // create an array of TV shows (mock data)
+        val favouriteTVShows = arrayOf(
+            TVShow("House of the Dragon", "HBO"),
+            TVShow("Lord of the Rings", "Prime Video"),
+            TVShow("Andor", "Disney"),
+            TVShow("Severance", "AppleTv"),
+            TVShow("Star Trek: Strange New Worlds", "Paramount+")
+        )
+
+        // create an instance of the FirstAdapter and pass in the array of TV shows
+        val firstAdapter = FirstAdapter(favouriteTVShows)
+        binding.firstRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = firstAdapter
+        }
 
     }
 }
