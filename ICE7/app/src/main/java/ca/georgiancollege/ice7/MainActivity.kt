de194ce7589a7034ce19a7ea1526d5ec2cad9353
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-02 15:52:07 -0400

    updated Main Activity

--- ICE7/app/src/main/java/ca/georgiancollege/ice7/MainActivity.kt
+++ ICE7/app/src/main/java/ca/georgiancollege/ice7/MainActivity.kt
@@ -1,38 +1,56 @@
 package ca.georgiancollege.ice7
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import ca.georgiancollege.ice7.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity()
 {
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var dataManager: DataManager
+
+    // Adapter for the RecyclerView, with a click listener to open the DetailsActivity
+    private val adapter = TVShowListAdapter { tvShow: TVShow ->
+        val intent = Intent(this, DetailsActivity::class.java).apply {
+            putExtra("tvShowId", tvShow.id)
+        }
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*
-        // create an array of TV shows (mock data)
-        val favouriteTVShows = arrayOf(
-            TVShow("House of the Dragon", "HBO"),
-            TVShow("Lord of the Rings", "Prime Video"),
-            TVShow("Andor", "Disney"),
-            TVShow("Severance", "AppleTv"),
-            TVShow("Star Trek: Strange New Worlds", "Paramount+")
-        )
+        dataManager = DataManager.instance(this)
 
-        // create an instance of the FirstAdapter and pass in the array of TV shows
-        val firstAdapter = FirstAdapter(favouriteTVShows)
-        binding.firstRecyclerView.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = firstAdapter
+        binding.firstRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.firstRecyclerView.adapter = adapter
+
+        loadTVShows()
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this, DetailsActivity::class.java)
+            startActivity(intent)
         }
+    }
 
-         */
+    override fun onResume()
+    {
+        super.onResume()
+        loadTVShows()
+    }
 
+    private fun loadTVShows()
+    {
+        lifecycleScope.launch {
+            val tvShows = dataManager.getAllTVShows()
+            adapter.submitList(tvShows)
+        }
     }
 }
