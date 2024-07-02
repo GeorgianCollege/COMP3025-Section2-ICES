16fa5f660902f9eaa0a4a1aa508d781781edc841
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-02 15:14:07 -0400

    added TVShowListAdapater

--- ICE7/app/src/main/java/ca/georgiancollege/ice7/TVShowListAdapter.kt
+++ ICE7/app/src/main/java/ca/georgiancollege/ice7/TVShowListAdapter.kt
@@ -1,8 +1,29 @@
 package ca.georgiancollege.ice7
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import ca.georgiancollege.ice7.databinding.TextRowItemBinding
 
 class TVShowListAdapter(private val onItemClicked: (TVShow) -> Unit):
     ListAdapter<TVShow, TVShowViewHolder>(TVShowComparator())
 {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TVShowViewHolder
+        {
+            val binding = TextRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return TVShowViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: TVShowViewHolder, position: Int)
+        {
+            val current = getItem(position)
+
+            holder.bind(current)
+
+            holder.itemView.setOnClickListener {
+                onItemClicked(current)
+            }
+        }
+
 }
