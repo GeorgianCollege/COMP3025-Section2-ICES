36dabe88e29055bbf1dec3ff9b1453d3c1291af0
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-02 22:10:56 -0400

    fixed issue with list view

--- ICE7/app/src/main/java/ca/georgiancollege/ice7/TVShowListAdapter.kt
+++ ICE7/app/src/main/java/ca/georgiancollege/ice7/TVShowListAdapter.kt
@@ -5,25 +5,28 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import ca.georgiancollege.ice7.databinding.TextRowItemBinding
 
-class TVShowListAdapter(private val onItemClicked: (TVShow) -> Unit):
-    ListAdapter<TVShow, TVShowViewHolder>(TVShowComparator())
-{
+// TVShowListAdapter is an adapter for the RecyclerView that displays a list of TVShow items.
+// It extends ListAdapter, which helps with efficiently updating the list using the TVShowComparator.
+class TVShowListAdapter(private val onItemClicked: (TVShow) -> Unit) :
+    ListAdapter<TVShow, TVShowViewHolder>(TVShowComparator()) {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TVShowViewHolder
-        {
-            val binding = TextRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return TVShowViewHolder(binding)
+    // Called when the RecyclerView needs a new ViewHolder to represent an item.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TVShowViewHolder {
+        // Inflate the item layout and create a binding object
+        val binding = TextRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        // Return a new ViewHolder instance with the binding object
+        return TVShowViewHolder(binding)
+    }
+
+    // Called by the RecyclerView to display the data at the specified position.
+    override fun onBindViewHolder(holder: TVShowViewHolder, position: Int) {
+        // Get the TVShow item at the given position
+        val current = getItem(position)
+        // Bind the TVShow item to the ViewHolder
+        holder.bind(current)
+        // Set a click listener on the itemView to handle item clicks
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
         }
-
-        override fun onBindViewHolder(holder: TVShowViewHolder, position: Int)
-        {
-            val current = getItem(position)
-
-            holder.bind(current)
-
-            holder.itemView.setOnClickListener {
-                onItemClicked(current)
-            }
-        }
-
+    }
 }
--- ICE7/app/src/main/java/ca/georgiancollege/ice7/TVShowViewHolder.kt
+++ ICE7/app/src/main/java/ca/georgiancollege/ice7/TVShowViewHolder.kt
@@ -3,12 +3,16 @@ package ca.georgiancollege.ice7
 import androidx.recyclerview.widget.RecyclerView
 import ca.georgiancollege.ice7.databinding.TextRowItemBinding
 
-class TVShowViewHolder(private val binding: TextRowItemBinding): RecyclerView.ViewHolder(binding.root)
-{
-    fun bind(tvShow: TVShow)
-    {
+// TVShowViewHolder is a ViewHolder for the RecyclerView that displays a single TVShow item.
+class TVShowViewHolder(private val binding: TextRowItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    // Binds the data from a TVShow object to the views in the ViewHolder.
+    fun bind(tvShow: TVShow) {
+        // Set the title of the TV show to the TextView
         binding.title.text = tvShow.title
+        // Set the genre of the TV show to the TextView
         binding.genre.text = tvShow.genre
+        // Set the rating of the TV show to the TextView
         binding.rating.text = tvShow.rating.toString()
     }
 }
