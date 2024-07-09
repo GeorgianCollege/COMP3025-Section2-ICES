ee4b164284d008c1ebf2ccd1232e8d03981747f4
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-09 14:11:39 -0400

    updated MainActivity to use ViewModel

--- ICE8/app/src/main/java/ca/georgiancollege/section2_ice8/MainActivity.kt
+++ ICE8/app/src/main/java/ca/georgiancollege/section2_ice8/MainActivity.kt
@@ -2,15 +2,18 @@ package ca.georgiancollege.section2_ice8
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import ca.georgiancollege.section2_ice8.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity()
 {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: TVShowViewModel by viewModels()
 
     private lateinit var dataManager: DataManager
 
@@ -27,13 +30,22 @@ class MainActivity : AppCompatActivity()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        dataManager = DataManager.instance(this)
+        // Initialize Firestore and DataManager
+        FirebaseFirestore.setLoggingEnabled(true)
+        dataManager = DataManager.instance()
 
         binding.firstRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.firstRecyclerView.adapter = adapter
 
-        loadTVShows()
+        // Observe the LiveData from the ViewModel to update the UI
+        viewModel.tvShows.observe(this) { tvShows ->
+            adapter.submitList(tvShows)
+        }
 
+        // Load all TV Shows from the database when the app starts
+        viewModel.loadAllTVShows()
+
+        // TODO: replace the addButton with a FAB
         binding.addButton.setOnClickListener {
             val intent = Intent(this, DetailsActivity::class.java)
             startActivity(intent)
@@ -43,14 +55,6 @@ class MainActivity : AppCompatActivity()
     override fun onResume()
     {
         super.onResume()
-        loadTVShows()
-    }
-
-    private fun loadTVShows()
-    {
-        lifecycleScope.launch {
-            val tvShows = dataManager.getAllTVShows()
-            adapter.submitList(tvShows)
-        }
+       viewModel.loadAllTVShows()
     }
 }
