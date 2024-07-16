75b29631c0ec43cd19473644d02cc815c7f52183
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-16 13:52:17 -0400

    added code for the Login Activity

--- ICE9/app/src/main/java/ca/georgiancollege/section2_ice9/LoginActivity.kt
+++ ICE9/app/src/main/java/ca/georgiancollege/section2_ice9/LoginActivity.kt
@@ -1,16 +1,62 @@
 package ca.georgiancollege.section2_ice9
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import ca.georgiancollege.section2_ice9.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity()
 {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var viewModel: TVShowViewModel
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        viewModel = ViewModelProvider(this).get(TVShowViewModel::class.java)
+
+        binding.loginButton.setOnClickListener{
+            val email = binding.emailAddressEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty())
+            {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if(task.isSuccessful)
+                        {
+                            val user = auth.currentUser
+                            user?.let {
+                                viewModel.loadUserById(it.uid)
+                            }
+
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        }
+                        else
+                        {
+                            Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+            else
+            {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        binding.registerButton.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
