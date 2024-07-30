c68db59ca1145e9b5defcbd9cba4037d2d387170
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-07-30 13:22:41 -0400

    changed input to touches for Player

--- ICE11/Assets/[Scripts]/PlayerBehaviour.cs
+++ ICE11/Assets/[Scripts]/PlayerBehaviour.cs
@@ -5,6 +5,12 @@ public class PlayerBehaviour : MonoBehaviour
     public float max;
     public float min;
     public float horizontalSpeed;
+    public float verticalPosition;
+
+    void Start()
+    {
+        transform.position = new Vector3(0.0f, verticalPosition, 0.0f);
+    }
 
     void Update()
     {
@@ -14,19 +20,29 @@ public class PlayerBehaviour : MonoBehaviour
 
     void Move()
     {
-        var x = Input.GetAxisRaw("Horizontal") * horizontalSpeed * Time.deltaTime;
-        transform.position += new Vector3(x, 0.0f, 0.0f);
+        if (Input.touchCount > 0)
+        {
+            // gets input touches from screen space in pixels
+            var x = Input.touches[0].position.x;
+
+            // converts screen space to world space
+            var horizontalPosition = Camera.main.ScreenToWorldPoint(new Vector3(x, 0.0f, 0.0f)).x;
+
+            transform.position = new Vector3(horizontalPosition, verticalPosition, 0.0f);
+        }
+
+        
     }
 
     void CheckBounds()
     {
         if (transform.position.x <= min)
         {
-            transform.position = new Vector3(min, -4.0f, 0.0f);
+            transform.position = new Vector3(min, verticalPosition, 0.0f);
         }
         else if (transform.position.x >= max)
         {
-            transform.position = new Vector3(max, -4.0f, 0.0f);
+            transform.position = new Vector3(max, verticalPosition, 0.0f);
         }
     }
 }
